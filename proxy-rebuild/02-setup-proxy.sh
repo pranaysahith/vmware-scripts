b806ebb4857b29add002f92d37645eba9313d8d9
Pranay <pranaysahith7@gmail.com>
2020-12-15 15:07:00 +0000

    add input params for gov.uk and wordpress sites

--- proxy-rebuild/02-setup-proxy.sh
+++ proxy-rebuild/02-setup-proxy.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
-if [ -z $1 ]
-then
-  echo "Please give IP address of ICAP as argument"
-  echo "Usage: $0 <ICAP server IP address> "
+read -p "Enter the IP address of ICAP server: " ICAP_IP
+ICAP_IP=${ICAP_IP}
+
+if [[ -z "$ICAP_IP" ]]; then
+  echo "Please pass IP address of ICAP server."
   exit -1
-else
-  ICAP_IP=$1
 fi
+
+read -p "Enter the IP address of gov.uk website [151.101.0.144] : " GOVUK_IP
+GOVUK_IP=${GOVUK_IP:-151.101.0.144}
+GOVUK_IP=$(echo $GOVUK_IP | sed 's|\.|\\.|g')
+
+read -p "Enter the IP address of wordpress website [192.0.78.17] : " WORDPRESS_IP
+WORDPRESS_IP=${WORDPRESS_IP:-192.0.78.17}
+WORDPRESS_IP=$(echo $WORDPRESS_IP | sed 's|\.|\\.|g')
+
+key=$(cat ./server.key | base64 | tr -d '\n')
+crt=$(cat ./server.crt | base64 | tr -d '\n')
 
 helm upgrade --install \
 --set image.nginx.repository=pranaysahith/reverse-proxy-nginx \
 --set image.nginx.tag=0.0.1 \
 --set image.squid.repository=pranaysahith/reverse-proxy-squid \
---set image.squid.tag=0.0.7 \
---set application.nginx.env.ALLOWED_DOMAINS='glasswallsolutions.com\,www.glasswallsolutions.com\,example.local\,www.example.local\,gov.uk\,www.gov.uk\,owasp.org\,www.owasp.org' \
+--set image.squid.tag=0.0.8 \
+--set application.nginx.env.ALLOWED_DOMAINS='glasswallsolutions.com\,www.glasswallsolutions.com\,example.local\,www.example.local\,gov.uk\,www.gov.uk\,assets.publishing.service.gov.uk\,owasp.org\,www.owasp.org' \
 --set application.nginx.env.ROOT_DOMAIN='glasswall-icap.com' \
 --set application.nginx.env.SUBFILTER_ENV='' \
---set application.squid.env.ALLOWED_DOMAINS='glasswallsolutions.com\,www.glasswallsolutions.com\,example.local\,www.example.local\,gov.uk\,www.gov.uk\,owasp.org\,www.owasp.org' \
+--set application.squid.env.ALLOWED_DOMAINS='glasswallsolutions.com\,www.glasswallsolutions.com\,example.local\,www.example.local\,gov.uk\,www.gov.uk\,assets.publishing.service.gov.uk\,owasp.org\,www.owasp.org' \
 --set application.squid.env.ROOT_DOMAIN='glasswall-icap.com' \
 --set application.squid.env.ICAP_URL="icap://$ICAP_IP:1344/gw_rebuild" \
 --set application.squid.env.ICAP_ALLOW_ONLY_MIME_TYPE='application/pdf' \
---set service.nginx.additionalHosts={"glasswallsolutions.com"\,"www.glasswallsolutions.com"\,"example.local"\,"www.example.local"\,"gov.uk"\,"www.gov.uk"\,"owasp.org"\,"www.owasp.org"} \
---set hostAliases."78\\.159\\.113\\.39"={"www.example.local"\,"example.local"} \
---set hostAliases."151\\.101\\.0\\.144"={"www.gov.uk"\,"gov.uk"} \
+--set service.nginx.additionalHosts={"glasswallsolutions.com"\,"www.glasswallsolutions.com"\,"example.local"\,"www.example.local"\,"gov.uk"\,"www.gov.uk"\,"owasp.org"\,"www.owasp.org"\,"assets.publishing.service.gov.uk"} \
+--set hostAliases.\""$WORDPRESS_IP"\"={"www.example.local"\,"example.local"} \
+--set hostAliases.\""$GOVUK_IP"\"={"www.gov.uk"\,"gov.uk"\,"assets.publishing.service.gov.uk"} \
+--set ingress.tls.crt=$crt \
+--set ingress.tls.key=$key \
 reverse-proxy /home/glasswall/s-k8-proxy-rebuild/stable-src/chart/
