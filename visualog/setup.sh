ce4887be1128bb657cd4563b95fed41370183c29
Faisal Adnan <68223517+faisaladnanpeltops@users.noreply.github.com>
2020-12-16 15:26:12 +0100

    minor update

--- visualog/setup.sh
+++ visualog/setup.sh
@@ -3,7 +3,7 @@
 # This is a placeholder script, you can move your setup script here to install some custom deployment on the VM
 # The parent directory of this script will be transferred with its content to the VM under /tmp/setup path
 # (i.e: useful for copying configs, scripts, systemd units, etc..)  
-git clone https://github.com/k8-proxy/vmware-scripts.git
+git clone --single-branch --depth 1 https://github.com/k8-proxy/vmware-scripts.git
 cd vmware-scripts/visualog/automation
 sh install_elk.sh
 sh install_kibana.sh
