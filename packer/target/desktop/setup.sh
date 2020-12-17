03337f4caed31734c453f92cbf3f3b547fba187c
NourEddineX <noureddinex@protonmail.com>
2020-12-17 18:51:21 +0200

    network config

--- packer/target/desktop/setup.sh
+++ packer/target/desktop/setup.sh
@@ -3,3 +3,6 @@ DEBIAN_FRONTEND=noninteractive
 sleep 3
 sudo apt install -y ubuntu-desktop
 sudo systemctl set-default graphical.target
+sudo sed -i '/network/ a \ \ renderer: NetworkManager' /etc/netplan/*.yaml
+sudo sed -i 's/managed=false/managed=true/g' /etc/NetworkManager/NetworkManager.conf
+sudo netplan apply
