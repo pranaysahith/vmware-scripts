7c172290e72fda72714ad7bf4e6e6723d6fd4512
Pranay <pranaysahith7@gmail.com>
2020-12-20 06:21:09 +0000

    install rancher cluster

--- k8s-base/setup.sh
+++ k8s-base/setup.sh
@@ -5,6 +5,8 @@
 # (i.e: useful for copying configs, scripts, systemd units, etc..)  
 
 set -e
+sudo usermod -aG docker ubuntu
+
 # install rancher
 sudo docker run -d --restart=unless-stopped \
   -p 8080:80 -p 8443:443 \
@@ -24,7 +26,73 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install helm -y
 echo "Done installing helm"
 # create cluster
+# Check if server is alive
+# -----------------------
+STATUS=`curl 'https://localhost:8443/ping' 2>/dev/null --insecure`
+echo $STATUS
+if [ "$STATUS" != "pong" ]
+then
+      echo "Rancher is not running. Aborting..."
+      exit 1
+fi
 
+SERVER_NAME=172.17.0.1
+CLUSTER_NAME=glasswall-proxy1
+ADMIN_PASSWORD=changepassword
+# Get auth token
+# -----------------------
+
+# Login
+LOGINRESPONSE=`curl -s 'https://127.0.0.1:8443/v3-public/localProviders/local?action=login' -H 'content-type: application/json' --data-binary '{"username":"admin","password":"admin"}' --insecure`
+# LOGINRESPONSE=`curl -s 'https://127.0.0.1:8443/v3-public/localProviders/local?action=login' -H 'content-type: application/json' --data-binary '{"username":"admin","password":"changepassword"}' --insecure`
+LOGINTOKEN=`echo $LOGINRESPONSE | jq -r .token`
+echo $LOGINTOKEN
+
+# Change admin Password
+# ----------------------
+curl -s 'https://127.0.0.1:8443/v3/users?action=changepassword' -H 'content-type: application/json' -H "Authorization: Bearer $LOGINTOKEN" --data-binary "{\"currentPassword\":\"admin\",\"newPassword\":\"$ADMIN_PASSWORD\"}" --insecure
+
+# Create API key
+# ----------------------
+APIRESPONSE=`curl -s 'https://127.0.0.1:8443/v3/token' -H 'content-type: application/json' -H "Authorization: Bearer $LOGINTOKEN" --data-binary '{"type":"token","description":"automation"}' --insecure`
+
+# Extract and store token
+# ----------------------
+APITOKEN=`echo $APIRESPONSE | jq -r .token`
+
+# Configure server-url
+# ----------------------
+RANCHER_SERVER="https://$SERVER_NAME:8443"
+curl -s 'https://127.0.0.1:8443/v3/settings/server-url' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" -X PUT --data-binary "{\"name\":\"server-url\",\"value\":\"$SERVER_NAME\"}" --insecure
+
+# Create cluster
+# ----------------------
+CLUSTERRESPONSE=`curl -s 'https://127.0.0.1:8443/v3/cluster' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --data-binary "{\"type\":\"cluster\",\"nodes\":[],\"rancherKubernetesEngineConfig\":{\"ignoreDockerVersion\":true},\"name\":\"$CLUSTER_NAME\"}" --insecure`
+
+# Extract clusterid to use for generating the docker run command
+# ----------------------
+CLUSTERID=`echo $CLUSTERRESPONSE | jq -r .id`
+# Get nodeCommand to add Master nodes
+# -------------------------------------
+
+# TODO: Try to quit the sed, and get the right server name
+
+# Specify role flags to use
+ROLEFLAGS="--etcd --controlplane --worker --address 172.17.0.1"
+
+# Generate token (clusterRegistrationToken) and extract nodeCommand
+AGENTCOMMAND=`curl -s 'https://127.0.0.1:8443/v3/clusterregistrationtoken' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --data-binary '{"type":"clusterRegistrationToken","clusterId":"'$CLUSTERID'"}' --insecure | jq -r .nodeCommand`
+
+# Show the command
+echo "Running command to register node: '${AGENTCOMMAND} ${ROLEFLAGS}'" sed  "s|--server|--server $RANCHER_SERVER|g"
+
+# Show the command
+final_agent_command=$(echo "${AGENTCOMMAND} ${ROLEFLAGS}" | sed  "s|--server|--server $RANCHER_SERVER|g")
+echo $final_agent_command
+eval $final_agent_command
+
+docker ps
+docker logs -f e9216d8f3526
 
 # create kubeconfig file
 
