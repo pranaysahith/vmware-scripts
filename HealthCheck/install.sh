a4a05acc8d712bc2c044d1cd3ec36151a4f4cf97
Mariusz Ferdyn <MariuszFerdyn@users.noreply.github.com>
2020-12-21 07:50:18 +0100

    Update install.sh

--- HealthCheck/install.sh
+++ HealthCheck/install.sh
@@ -6,4 +6,4 @@ get https://raw.githubusercontent.com/MariuszFerdyn/vmware-scripts/main/HealthCh
 chmod +x pyCheck.py
 get https://raw.githubusercontent.com/MariuszFerdyn/vmware-scripts/main/HealthCheck/config.yml config.yml
 get https://raw.githubusercontent.com/MariuszFerdyn/vmware-scripts/main/HealthCheck/a.pdf a.pdf
-apt-get -y installl c-icap
+sudo apt-get -y installl c-icap
