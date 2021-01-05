4605ae07481e36a1815d5a5641ff632e071ac080
NourEddineX <noureddinex@protonmail.com>
2021-01-05 22:04:01 +0200

    update submodules

--- packer/target/filedrop/setup.sh
+++ packer/target/filedrop/setup.sh
@@ -4,6 +4,9 @@ bash <( curl -sfL https://get.k3s.io )
 bash <( curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 )
 mkdir ~/.kube && sudo install -T /etc/rancher/k3s/k3s.yaml ~/.kube/config -m 600 -o $USER
 git clone --single-branch https://github.com/k8-proxy/k8-rebuild
+pushd k8-rebuild/
+git submodule update --init --recursive --progress
+popd
 sed -i.bak "/NodePort/ a \    nodePorts:\n      http: 30080" k8-rebuild/kubernetes/values.yaml 
 cat >> k8-rebuild/kubernetes/values.yaml <<EOF
 
@@ -25,3 +28,4 @@ helm install sow-rest k8-rebuild/kubernetes/
 echo -e "\n\n############################\n\n"
 echo "Visit http://$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p'):30080"
 echo -e "\n\n############################\n\n"
+
