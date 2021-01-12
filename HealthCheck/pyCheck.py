004211838b35e2ff2fd3cb17aee834d87d44462b
Mariusz Ferdyn <mf@fast-sms.net>
2021-01-12 12:57:44 +0100

    ### New Features 2021-01-12 * fixed Round Trip Time * Output to console - set in config.yml * Output to file - set in config.yml * Output to syslog - set in config.yml

--- HealthCheck/pyCheck.py
+++ HealthCheck/pyCheck.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 import yaml
 import ipaddress
@@ -7,66 +9,78 @@ import socket
 import requests
 import re
 import logging
+import logging.handlers
 import random
 import string
 import hashlib
 import os
 import time
-import pathlib
-from datetime import datetime
-import netifaces as ni
+import platform
 
-def get_default_gateway():
-    """
-    Attempts to read /proc/self/net/route to determine the default gateway in>
-    :return: String - the network interface of the default gateway or None if not fo>
-    """
-    try:
-        hip = None
-        # The first line is the header line
-        # We look for the line where the Destination is 00000000 - that is th>
-        # The Gateway interface is encoded front of.
-        with open("/proc/self/net/route") as routes:
-            for line in routes:
-                parts = line.split('\t')
-                if '00000000' == parts[1]:
-                    hip = parts[0]
-        return hip
-    except Exception:
-        logger.warning("get_default_gateway: ", exc_info=True)
+log_dest = 'console'
+logger = None
 
 def checksum(filename, hashfunc):
-    with open(filename, "rb") as f:
-        for byte_block in iter(lambda: f.read(4096), b""):
+    with open(filename,"rb") as f:
+        for byte_block in iter(lambda: f.read(4096),b""):
             hashfunc.update(byte_block)
     return hashfunc.hexdigest()
 
+def log(log, end='\n', flush=True):
+    if log_dest == 'file' or log_dest == 'syslog':
+        logger.info(log)
+    else:
+        print(log, end=end, flush=flush)
 
 def main(args):
-    retcode = 0
-# Get public ip address of host
-    iface = get_default_gateway()
-    ni.ifaddresses(iface)
-    ipaddr = ni.ifaddresses(iface)[ni.AF_INET][0]['addr'] # use IP address in log result , incase use hostname, please uncomment-line ipaddr = socket.gethostname() below
-#Get Hostname
-#    ipaddr = socket.gethostname()
+    global log_dest
+    global logger
 
-    FAIL = "FAIL"
-    PASS = "PASS"
+    retcode = 0
+    config = None
+
     SSLVerify = False
     logging.captureWarnings(True)
-    # SonHa : Start fix hardcode path
-    with open(pathlib.Path(pathlib.Path(__file__).parent.absolute(), 'config.yml')) as file:
-        # SonHa: End fix hardcode path
-        config = yaml.load(file, Loader=yaml.Loader)
-# Read data from config.yml file and execute commands such as ping, https, icap ...
-# Output format like this :
-# -> Dec 23 12:11:43 78.159.113.57 ping 8.8.8.8: PASS 13.23
-# Dec 23 12:11:43 : timestamp
-# 78.159.113.57: host
-# ping 8.8.8.8: checkpoint
-# PASS: result
-# 13.23: round trip time in ms
+
+    for f in args + [ os.path.dirname(__file__) + '/config.yml' ]:
+        try:
+            with open(f) as file:
+                config = yaml.load(file, Loader=yaml.Loader)
+                break
+        except OSError:
+            pass
+
+    if not config:
+        logging.critical('invalid config')
+        return -1
+
+    try:
+        log_dest = config['config']['log']
+    except KeyError:
+        log_dest = 'console'
+    if log_dest == 'syslog':
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
+        logger.addHandler(logging.handlers.SysLogHandler(address='/dev/log'))
+    elif log_dest == 'file':
+        try:
+            logfile = config['config']['logfile']
+        except KeyError:
+            log_dest = 'console'
+            logging.error('logfile not defined')
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
+        fh = logging.FileHandler(filename=logfile)
+        hastname = platform.node()
+        fh.setFormatter(logging.Formatter(f'%(asctime)s {platform.node()}: %(message)s'))
+        logger.addHandler(fh)
+
+
+    istty = log_dest == 'console' and sys.stdout.isatty()
+
+    FAIL = "\033[1;91m" + "FAIL" + "\033[0m" if istty else "FAIL"
+    PASS = "\033[1;92m" + "PASS" + "\033[0m" if istty else "PASS"
+
     for i in config['hosts']:
         try:
             addr = i['address']
@@ -74,24 +88,28 @@ def main(args):
                 addr = str(ipaddress.ip_address(addr))
                 url = urllib.parse.urlparse(f'http://{addr}')
             except ValueError:
-                url = urllib.parse.urlparse(addr, scheme='http')
-                if url.netloc == '' and url.path != '':
+                url = urllib.parse.urlparse(addr,scheme='http')
+                if url.netloc=='' and url.path != '':
                     url = urllib.parse.urlparse(f'{url.scheme}://{url.path}')
-                addr = url.hostname
+                addr =  url.hostname
         except KeyError:
             continue
-        now = datetime.now()
+
         if i['prot'] == 'icmp':
-            print(f'{now:%b %d %H:%M:%S} {ipaddr} ping {addr}: ',end='', flush=True)
+            msg_format = f'{i["prot"]:12}{addr:30}: ' if istty else f'{i["prot"]} {addr}: '
+            if istty: print(msg_format, end='', flush=True)
             start = time.perf_counter()
-            cp = subprocess.run(['ping', '-c1', '-w2', addr], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            cp = subprocess.run(['ping','-c1','-w2',addr],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
             end = time.perf_counter()
-            PASS_TIME = PASS + f' {((end - start) * 1000):.2f}'
-            print(f'{FAIL if cp.returncode else PASS_TIME}')
+            PASS_TIME = PASS + f' {((end-start)*1000):.2f}ms '
+            result = f'{FAIL if cp.returncode else PASS_TIME}'
+            if istty: print(result)
+            else: log(msg_format + result)
             retcode = retcode + cp.returncode
         elif i['prot'] == "tcp":
-         #   print(f'tcp/{i["tcpport"]:<6} {addr:30}: ', end='', flush=True)
-            print(f'{now:%b %d %H:%M:%S} {ipaddr} tcp/{i["tcpport"]:<6} {addr}: ',end='', flush=True)
+            i['prot'] = f'tcp/{i["tcpport"]}'
+            msg_format = f'{i["prot"]:12}{addr:30}: ' if istty else f'{i["prot"]} {addr}: '
+            if istty: print(msg_format, end='', flush=True)
             s = None
             for res in socket.getaddrinfo(addr, i['tcpport'], socket.AF_UNSPEC, socket.SOCK_STREAM):
                 af, socktype, proto, canonname, sa = res
@@ -110,64 +128,81 @@ def main(args):
                     continue
                 break
             end = time.perf_counter()
-            PASS_TIME = PASS + f' {((end - start) * 1000):.2f}'
-            print(f'{PASS_TIME if s else FAIL}')
+            PASS_TIME = PASS + f' {((end-start)*1000):.2f}ms '
+            result = f'{PASS_TIME if s else FAIL}'
+            if istty: print(result)
+            else: log(msg_format + result)
             retcode = retcode + (0 if s else 1)
             if s: s.close()
         elif i['prot'] == 'httpstatus':
-            #print(f'httpstatus {url.geturl():30}: ', end='', flush=True)
-            print(f'{now:%b %d %H:%M:%S} {ipaddr} httpstatus {url.geturl()}: ',end='', flush=True)
+            addr = url.geturl()
+            msg_format = f'{i["prot"]:12}{addr:30}: ' if istty else f'{i["prot"]} {addr}: '
+            if istty: print(msg_format, end='', flush=True)
             start = time.perf_counter()
             try:
-                r = requests.get(url.geturl(), verify=SSLVerify, timeout=5)
+                r = requests.get(addr, verify=SSLVerify, timeout=5)
             except requests.exceptions.ConnectTimeout:
                 r = None
             except requests.exceptions.ConnectionError:
                 r = None
             end = time.perf_counter()
-            PASS_TIME = PASS + f' {((end - start) * 1000):.2f}'
-            print(f'{PASS_TIME if r and r.status_code == i["httpstatus"] else FAIL}')
-            retcode = retcode + (0 if r and r.status_code == i["httpstatus"] else 1)
+            PASS_TIME = PASS + f' {((end-start)*1000):.2f}ms '
+            result = f'{PASS_TIME if r and r.status_code==i["httpstatus"] else FAIL}'
+            if istty: print(result)
+            else: log(msg_format + result)
+            retcode = retcode + (0 if r and r.status_code==i["httpstatus"] else 1)
         elif i['prot'] == 'httpstring':
-            #print(f'httpstring {url.geturl():30}: ', end='', flush=True)
-            print(f'{now:%b %d %H:%M:%S} {ipaddr} httpstring {url.geturl()}: ',end='', flush=True)
+            addr = url.geturl()
+            msg_format = f'{i["prot"]:12}{addr:30}: ' if istty else f'{i["prot"]} {addr}: '
+            if istty: print(msg_format, end='', flush=True)
             start = time.perf_counter()
             try:
-                r = requests.get(url.geturl(), verify=SSLVerify, timeout=5)
+                r = requests.get(addr, verify=SSLVerify, timeout=5)
             except requests.exceptions.ConnectTimeout:
                 r = None
             except requests.exceptions.ConnectionError:
                 r = None
             end = time.perf_counter()
-            PASS_TIME = PASS + f' {((end - start) * 1000):.2f}'
-            print(f'{PASS_TIME if r and re.search(i["httpstring"], r.text) else FAIL}')
-            retcode = retcode + (0 if r and re.search(i["httpstring"], r.text) else 1)
+            PASS_TIME = PASS + f' {((end-start)*1000):.2f}ms '
+            result = f'{PASS_TIME if r and re.search(i["httpstring"],r.text) else FAIL}'
+            if istty: print(result)
+            else: log(msg_format + result)
+            retcode = retcode + (0 if r and re.search(i["httpstring"],r.text) else 1)
         elif i['prot'] == 'icap':
-            print(f'{now:%b %d %H:%M:%S} {ipaddr} icap     {addr}: ', end='', flush=True)
+            msg_format = f'{i["prot"]:12}{addr:30}: ' if istty else f'{i["prot"]} {addr}: '
+            if istty: print(msg_format, end='', flush=True)
             suffix = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
             try:
+                tmout = i['icaptimeout']
+            except KeyError:
+                tmout = 20
+            for f in [ i["icaptestfile"], os.path.dirname(__file__)+"/"+i["icaptestfile"] ]:
+                if os.path.isfile(f):
+                    i['icaptestfile'] = f
+                    break
+            try:
                 start = time.perf_counter()
-                cp = subprocess.run(['c-icap-client', '-i', addr, '-s', i["icapservice"], '-f', i["icaptestfile"], '-o',
-                                     i["icaptestfile"] + suffix], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,
-                                    timeout=i['icaptimeout'])
+                cp = subprocess.run(['c-icap-client','-i',addr,'-s',i["icapservice"],'-f',i["icaptestfile"],'-o',i["icaptestfile"]+suffix],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL,timeout=tmout)
                 end = time.perf_counter()
-                PASS_TIME = PASS + f' {((end - start) * 1000):.2f}'
+                PASS_TIME = PASS + f' {((end-start)*1000):.2f}ms '
             except subprocess.TimeoutExpired:
-                cp.returncode == 1
+                cp.returncode = 1
             if cp.returncode == 0:
-                if os.path.isfile(i['icaptestfile'] + suffix):
-                    c2 = checksum(i['icaptestfile'] + suffix, hashlib.md5())
-                    os.remove(i['icaptestfile'] + suffix)
-                    if checksum(i['icaptestfile'], hashlib.md5()) != c2:
-                        print(PASS_TIME)
+                if os.path.isfile(i['icaptestfile']+suffix):
+                    c2 = checksum(i['icaptestfile']+suffix,hashlib.md5())
+                    os.remove(i['icaptestfile']+suffix)
+                    if checksum(i['icaptestfile'],hashlib.md5()) != c2:
+                        if istty: print(PASS_TIME)
+                        else: log(msg_format + PASS_TIME)
                         continue
 
-            print(FAIL)
+            if istty: print(FAIL)
+            else: log(msg_format + FAIL)
             retcode += 1
             continue
 
     return retcode
 
-
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
+
